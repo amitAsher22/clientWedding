91415f47bd5559d4112a931ddfcd8b766b3ca943
amita <amitasher4@gmail.com>
2022-09-16 16:25:45 +0300

    create register and login

---
+++ src/components/Login.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import "../App.css";
+// import { createGlobalStyle } from "styled-components";
+
+function Login() {
+  const [data, setData] = useState({ email: "", password: "" });
+
+  const collectionLogin = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value });
+    console.log(data);
+  };
+
+  return (
+    <LoginDiv>
+      <SecondDivLogin>
+        <Input
+          placeholder="Email"
+          name="email"
+          onChange={(e) => collectionLogin(e)}
+        />
+        <Input
+          placeholder="Password"
+          name="password"
+          onChange={(e) => collectionLogin(e)}
+        />
+        <BtnLogin>Log In</BtnLogin>
+        <Hrstyle />
+        <Link to="/register">
+          <CreateAccountBtn>Create new account</CreateAccountBtn>
+        </Link>
+      </SecondDivLogin>
+    </LoginDiv>
+  );
+}
+
+export default Login;
+
+const LoginDiv = styled.div`
+  ackground-color: #fff;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1), 0 9px 10px rgba(0, 0, 0, 0.1);
+  width: 30%;
+  height: 80%;
+  padding: 1.5em;
+  margin: auto;
+`;
+
+const SecondDivLogin = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.8rem;
+  margin: auto;
+`;
+
+const Input = styled.input`
+  padding: 0.9rem;
+  font-size: 1rem;
+  background-color: #d7e0ec;
+  color: black;
+  border: 0.1em solid gray;
+  border-radius: 0.7rem;
+`;
+
+const BtnLogin = styled.button`
+  padding: 0.9rem;
+  font-size: 1.5rem;
+  color: white;
+  background-color: #4267b2;
+  border: none;
+  border-radius: 0.7rem;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Hrstyle = styled.hr`
+  width: 100%;
+  font-size: 0.2rem;
+`;
+
+const CreateAccountBtn = styled.button`
+  font-size: 1.3rem;
+  color: white;
+  width: 90%;
+  background-color: #42b72a;
+  border: none;
+  border-radius: 0.2rem;
+  margin: auto;
+  &:hover {
+    cursor: pointer;
+  }
+`;
---
+++ src/components/Register.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+function Register() {
+  const [data, setData] = useState({ email: "", password: "", name: "" });
+
+  const collectionData = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value });
+  };
+  return (
+    <LoginDiv>
+      <SecondDivLogin>
+        <Input
+          placeholder="Email"
+          name="email"
+          onChange={(e) => collectionData(e)}
+        />
+        <Input
+          placeholder="Name"
+          name="name"
+          onChange={(e) => collectionData(e)}
+        />
+        <Input
+          placeholder="Password"
+          name="password"
+          onChange={(e) => collectionData(e)}
+        />
+        <BtnLogin>register</BtnLogin>
+        <Hrstyle />
+        <Link to="/">
+          <LoginUser>Go Login</LoginUser>
+        </Link>
+      </SecondDivLogin>
+    </LoginDiv>
+  );
+}
+
+export default Register;
+
+const LoginDiv = styled.div`
+  ackground-color: #fff;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1), 0 9px 10px rgba(0, 0, 0, 0.1);
+  width: 30%;
+  height: 80%;
+  padding: 1.5em;
+  margin: auto;
+`;
+
+const SecondDivLogin = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.8rem;
+  margin: auto;
+`;
+
+const Input = styled.input`
+  padding: 0.9rem;
+  font-size: 1rem;
+  background-color: #d7e0ec;
+  color: black;
+  border: 0.1em solid gray;
+  border-radius: 0.7rem;
+`;
+
+const BtnLogin = styled.button`
+  padding: 0.9rem;
+  font-size: 1.5rem;
+  color: white;
+  background-color: orange;
+  border: none;
+  border-radius: 0.7rem;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Hrstyle = styled.hr`
+  width: 100%;
+  font-size: 0.2rem;
+`;
+
+const LoginUser = styled.button`
+  font-size: 1.3rem;
+  color: white;
+  width: 90%;
+  background-color: #42b72a;
+  border: none;
+  border-radius: 0.2rem;
+  margin: auto;
+  &:hover {
+    cursor: pointer;
+  }
+`;
