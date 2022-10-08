d14be73e36c31786af2e25f677806be85b313a69
amita <amitasher4@gmail.com>
2022-10-08 16:27:36 +0300

    create menu navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,39 @@
 import React from "react";
+import "../style/navbar.css";
 import { Link } from "react-router-dom";
 import { BsPersonCircle } from "react-icons/bs";
+import { FiLogIn } from "react-icons/fi";
+import { MdSystemUpdateAlt } from "react-icons/md";
 
 function Navbar() {
   return (
     <div>
-      <p>
-        <Link to="/login">login</Link>
-        <Link to="/Register">Register</Link>
-        <Link to="/">
-          <BsPersonCircle />
-        </Link>
-      </p>
+      <div className="container">
+        <Link>logo</Link>
+        <div className="middle">
+          <Link>גני אירועים</Link>
+          <Link>אולמות אירועים</Link>
+          <Link>שמלות כלה</Link>
+          <Link>צילום אירועים</Link>
+          <Link> דיג'יי</Link>
+        </div>
+
+        <div className="right">
+          <BsPersonCircle className="signMenu" />
+          <div className="dropDownMenu">
+            <span>
+              <Link to="/login">
+                <FiLogIn /> הירשם
+              </Link>
+            </span>
+            <span>
+              <Link to="/Register">
+                <MdSystemUpdateAlt /> התחברות
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
